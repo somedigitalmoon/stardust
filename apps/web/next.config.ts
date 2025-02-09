ac34d746fb1401a7ec49b9d21cf5c9fccd6f36e7
tgt <tgt@incognitotgt.me>
2025-02-09 14:41:34 -0500

    refactor: move to nodejs middleware for web

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -22,6 +22,7 @@ const nextConfig: NextConfig = {
 		webpackBuildWorker: true,
 		reactCompiler: true,
 		authInterrupts: true,
+		nodeMiddleware: true,
 		serverActions: {
 			allowedOrigins: ["localhost:3000", "*.use.devtunnels.ms"],
 		},
--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,15 +1,8 @@
-import { reqWithTrustedOrigin } from "@/lib/real-origin-req";
-import type { SessionSchema } from "@stardust/common/auth";
+import auth from "@stardust/common/auth";
 import { type NextRequest, NextResponse } from "next/server";
 const allowedPaths = ["/auth/signin", "/auth/error", "/auth/verify", "/auth/signup"];
 export default async function middleware(req: NextRequest) {
-	const sessionEndpoint = `${reqWithTrustedOrigin(req).nextUrl.origin}/api/auth/get-session`;
-	const res = await fetch(sessionEndpoint, {
-		headers: {
-			cookie: req.headers.get("cookie") || "",
-		},
-	});
-	const session: SessionSchema = await res.json();
+	const session = await auth.api.getSession({ headers: req.headers });
 	if (session || allowedPaths.includes(req.nextUrl.pathname)) {
 		return NextResponse.next();
 	}
@@ -19,4 +12,5 @@ export default async function middleware(req: NextRequest) {
 
 export const config = {
 	matcher: ["/((?!_next/static|_next/image|icon.svg|nostr|api/auth|manifest.webmanifest).*)"],
+	runtime: "nodejs",
 };
